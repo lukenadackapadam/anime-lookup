3bb67ef8998840981ea4c0a905084b873661d612
luke <luke.nadackapadam@gmail.com>
2023-07-01 18:17:14 -0500

    add user_input to get response from api

--- main.rb
+++ main.rb
@@ -3,3 +3,11 @@ require "http"
 system "clear"
 
 puts "Please type the name of the anime you wish to learn more about."
+
+puts "\n"
+
+user_input = gets.chomp.downcase
+
+response = HTTP.get("https://api.jikan.moe/v4/anime?q=#{user_input}&sfw")
+
+pp response.parse(:json)
