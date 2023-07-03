df85906cfb700f821794a25c93f34f1ef197ae99
luke <luke.nadackapadam@gmail.com>
2023-07-02 19:25:58 -0500

    add method for titles with multiple words

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+def multiple_words(string)
+  array = string.split()
+  string_joined = array.join("+")
+  return string_joined
+end
+
 require "http"
 
 system "clear"
@@ -8,8 +14,15 @@ puts "\n"
 
 user_input = gets.chomp.downcase
 
+user_input = multiple_words(user_input)
+
 response = HTTP.get("https://api.jikan.moe/v4/anime?q=#{user_input}&sfw")
 
 title = response.parse(:json)["data"][0]["title"]
 
 synopsis = response.parse(:json)["data"][0]["synopsis"]
+
+puts "\n"
+puts "Title: #{title}"
+puts "\n"
+puts "Synopsis: \n#{synopsis}"
